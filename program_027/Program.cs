c36e56d9cc02420a730f65da16d07b1e274be4de
Vasiliy <vasiliy.trunov.00@mail.ru>
2022-08-21 01:31:31 +0200

    задача 27

---
+++ program_027/Program.cs
@@ -0,0 +1,18 @@
+﻿//  Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+int Sum(int n1){
+    int rt = 0;
+
+    while (n1>=1)
+    {
+        rt+=n1%10;
+        n1/=10;    
+    }
+    return rt;
+}
+
+System.Console.WriteLine("Введите число - ");
+int n1 = Math.Abs(int.Parse(System.Console.ReadLine()!));
+
+System.Console.WriteLine(Sum(n1));
+
